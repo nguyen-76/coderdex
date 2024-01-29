3dbc4855d95feca0061c5125c94daa4b38f3892d
Nguyen Doan <nguyendoan3456@gmail.com>
2024-01-29 03:44:17 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var express = require("express");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var pokemonsRouter = require("./routes/pokemons");
 
 var app = express();
+app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/pokemons", pokemonsRouter);
+
+app.use((error, req, res, next) => {
+  res.status(error.statusCode).send(error.message);
+});
 
 module.exports = app;
